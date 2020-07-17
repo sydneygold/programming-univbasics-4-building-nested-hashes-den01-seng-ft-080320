e98f7d4336a58c43e093e1dd5acbbcc7b8ce9f99
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-17 20:04:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,7 @@
 def base_hash
-  :railroads => {}
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-
+  base_hash => { :railroads => {}
+} 
+hash
 end
 
 def monopoly_with_second_tier
